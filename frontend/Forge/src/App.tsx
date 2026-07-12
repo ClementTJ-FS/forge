0cc44d4a446482496478633c98a84bac7f011af4
ClementTJ-FS <TMendoza1@student.fullsail.edu>
2026-07-11 19:52:42 -0700

    feat: initialize frontend with React and Vite setup
    - Added React and Vite configuration files.
    - Created main entry point for the application.
    - Introduced global CSS styles for light and dark themes. (vite default)
    - Included SVG assets for React and Vite logos.
    - Configured TypeScript settings for application and node environments.

---
+++ frontend/Forge/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>FORGE</h1>
+      <h2>An AI-powered portfolio, knowledge base, and engineering lab.</h2>
+      <p>This project is where I forge myself into a full-stack AI engineer.</p>
+    </>
+  )
+}
+
+export default App
